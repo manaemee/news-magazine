3f7dc42c389f4090b46c2426f3fa9b3ed80c2d0c
전민경 <jeonmingyeong@jeonmingyeong-ui-MacBookAir.local>
2022-03-11 11:36:46 +0900

    URL changed

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -109,7 +109,7 @@ function Overview(){
         <Subtitle>
         Find the latest breaking news and information on the top stories.
         </Subtitle>
-      <Link to={`${process.env.PUBLIC_URL}/all`}>
+      <Link to="all">
       <Button>
         <span>Start Now</span>
         <FontAwesomeIcon icon={faCheck}/>
